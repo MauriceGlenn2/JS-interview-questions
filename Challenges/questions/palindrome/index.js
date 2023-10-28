fab4cbc43d7fd72ab8c5b29a5141f160cbc20047
Maurice <mauriceglenn2@gmail.com>
2023-10-28 13:11:45 -0500

    Runing questions

--- Challenges/questions/palindrome/index.js
+++ Challenges/questions/palindrome/index.js
@@ -9,8 +9,50 @@
  * palindrome('frontend') === false
  */
 
+
+// Using reverse()
+// const palindrome = (str) => {
+//   let reversed =  str.split('').reverse().join('')
+//   return reversed === str
+
+// };
+
+
+//Using for of loop
+// const palindrome = (str) => {
+//     let reversed =  ''
+//     for (let char of str){
+//         reversed = char + reversed
+//     }
+//     return reversed === str
+// };
+
+
+//Use every() method, can only be used on arrays
+//this is inefficent 
+// const palindrome = (str) => {
+//     return str.split('').every((char, index) =>(
+//         char === str[str.length - 1 - index]))
+// };
+
+//*******************iterate over the first half of a string odd or even********************
+//efficent BEST WAY TO SOLVE
 const palindrome = (str) => {
-  
-};
+    const middle = Math.floor(str.length / 2) // iterate over first 2 indexes. 
+    for (let i = 0; i < middle; i++){
+      const oppositeIndex = str.length - 1 - i; //if current char not equal to opposite index
+      if (str[i] !== str[oppositeIndex]) {
+        return false;
+      }
+    }
+    return true
+}
+
+
+
+
+
+
+
 
 module.exports = palindrome;
--- Challenges/questions/reverseString/index.js
+++ Challenges/questions/reverseString/index.js
@@ -6,20 +6,71 @@
  * reverse('Simplified!') === '!deifilpmiS'
  */
 
+//0 - str[i] = d, reversed = 'd' + '' = 'd'
+//1 - str[i] = a, reversed = 'a' + 'd' = 'ad'
+//2 - str[i] = v, reversed = 'v' + 'ad' = 'vad'
+//3 - str[i] = i, reversed = 'i' + 'vad' = 'ivad'
+//4 - str[i] = d, reversed = 'd' + 'ivad' = 'divad'
+
+// const reverseString = (str) => {
+//     let reversed = ""
+//  for (let i = 0; i < str.length; i++ ){
+//     reversed = str[i] + reversed
+//  }
+//  return reversed
+// };
+// console.log(reverseString("david"));
+// module.exports = reverseString;
+
+/**For of loop */
+
+//0 - char = d, reversed = 'd' + '' = 'd'
+//1 - char = a, reversed = 'a' + 'd' = 'ad'
+//2 - char = v, reversed = 'v' + 'ad' = 'vad'
+//3 - char = i, reversed = 'i' + 'vad' = 'ivad'
+//4 - char = d, reversed = 'd' + 'ivad' = 'divad'
+// const reverseString = (str) => {
+//   let reversed = "";
+
+//   for (let char of str) {
+//     reversed = char + reversed
+//   }
+//   return reversed;
+// };
+// console.log(reverseString("david"));
+// module.exports = reverseString;
+
+/** Reverse */
+// const reverseString = (str) => {
+//  return str.split("").reverse().join("");
+
+// };
+// console.log(reverseString("david"));
+// module.exports = reverseString;
+
+/**Using REDUCE */
+//reduce can only be used with an array
+
+//adding elements in arr usiing reduce
+// const arr = [1, -1, 2, 3]
+
+// const sum = arr.reduce((accumlator, currentValue) => {
+//    return accumlator + currentValue
+// }, 0)
+
+// console.log(sum)
+
 // 'David' => ['D', 'a', 'v', 'i', 'd']
 // a = '', c = 'D' => a = 'D' + '' = 'D'
 // a = 'D', c = 'a' => a = 'a' + 'D' = 'aD'
 // a = 'aD', c = 'v' => a = 'v' + 'aD' = 'vaD'
 // a = 'vaD', c = 'i' => a = 'i' + 'vaD' = 'ivaD'
 // a = 'ivaD', c = 'd' => a = 'd' + 'ivaD' = 'divaD'
-
 const reverseString = (str) => {
-    let reversed = ""
- for (let i = 0; i < str.length; i++ ){
-    reversed = str[i] + reversed
- }
- return reversed
+  str.split("").reduce((accumlator, current) => {
+    return current + accumlator;
+  });
 };
-console.log(reverseString("david"));
 
+console.log(reverseString("david"));
 module.exports = reverseString;
