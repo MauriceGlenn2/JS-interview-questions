6bd94bff875feb1e614f6a825c0a55cf6871324b
Maurice <mauriceglenn2@gmail.com>
2023-10-29 11:18:18 -0500

    Solved reversed int

--- Challenges/questions/reverseInteger/index.js
+++ Challenges/questions/reverseInteger/index.js
@@ -7,8 +7,23 @@
  * reverseInt(-50) === -5
  */
 
-const reverseInteger = (int) => {
+// const reverseInteger = (int) => {
+//     let reversed =  parseInt(int.toString().split("").reverse().join(""));
+//     return int < 0 ? reversed * -1 : reversed 
+// }
 
+// const reverseInteger = (int) => {
+//   let reversed = parseInt(int.toString().split("").reverse().join(""));
+//   if (int < 0) {
+//     return reversed * -1;
+//   }
+//   return reversed;
+// };
+
+
+const reverseInteger = (int) => {
+  const reversed = int.toString().split("").reverse().join("");
+ return parseInt(reversed) * Math.sign(int); 
 };
 
 module.exports = reverseInteger;
