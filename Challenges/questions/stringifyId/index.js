66019eb48976f7028698c4b54ddd8e2649ae3a24
Maurice <mauriceglenn2@gmail.com>
2023-10-30 23:56:02 -0500

    Turing arrays into strings

--- Challenges/questions/stringifyId/index.js
+++ Challenges/questions/stringifyId/index.js
@@ -16,8 +16,62 @@
  * ]) === 'ABC, abc, CBA'
  */
 
+
+/**
+ * Convert the array of objects into an Array of ID;s
+ * 
+ * convert the array of Id's into an array of unique ID's
+ *
+ * join the array to creat a string of ID's separated by ", " 
+ * 
+ * get rid of last 2 char in a string slice(0, str.length - 2)
+ */
+
+//  const stringifyId = (arr) => {
+//     const idArray = arr.map(obj => obj.id)
+//     const uniqueArry =[]
+//     let idString = ""
+
+
+//     for (let id of idArray){
+//         if(!uniqueArry.includes(id)){
+//             uniqueArry.push(id)
+//         }
+//     }
+
+//     for (let i = 0; i < uniqueArry.length; i++){
+//         //if we are not on the last element
+//         if(i !== uniqueArry.length - 1){
+//           //add the id and ", "
+//           idString += `${uniqueArry[i]}, `;
+//         }
+//         else{
+//           //just add the id
+//           idString += uniqueArry[i];
+//         }
+        
+//     }
+    
+//     return idString 
+
+// };
+
+//top tech code
+// const stringifyId = (arr) => {
+//   const idArray = arr.map(obj => obj.id)
+//     const uniqueArry =[]
+//     for (let id of idArray){
+//         if(!uniqueArry.includes(id)){
+//             uniqueArry.push(id)
+//         }
+//     }
+//     return uniqueArry.join(", ")
+// }
+
+//using set
 const stringifyId = (arr) => {
 
-};
+}
+
 
 module.exports = stringifyId;
