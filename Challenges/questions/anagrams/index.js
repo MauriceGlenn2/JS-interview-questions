8a321af1e16dd9b7230399aca5556726cf22403e
Maurice <mauriceglenn2@gmail.com>
2023-12-01 12:32:34 -0600

    learning anagrams

--- Challenges/questions/anagrams/index.js
+++ Challenges/questions/anagrams/index.js
@@ -10,7 +10,45 @@
  */
 
 const anagrams = (strA, strB) => {
-  
+  //create 2 char maps, one for each string
+  const charMapA = buildCharMaps(strA);
+  const charMapB = buildCharMaps(strB);
+  //need to loop over char maps to compare
+
+  //if you have a char map that the is shorter than the other, but has the same chars, it will return true because
+  //length is not long enough
+  if (charMapA.size !== charMapB.size) return false;
+
+  //loops over every single char in char map A and compares the count to the same count in charmap B
+  //if they are not the same, return false
+  for (const [charA, countA] of charMapA) {
+    if (charMapB.get(charA) !== countA) return false;
+  }
+  return [charMapA, charMapB];
 }
 
+//create a helper function to remove spaces and lowercase, and for loops over char maps
+//never repeat code, use helper functions
+const buildCharMaps = (str) => {
+  const charMap = new Map();
+  for (const char of removeSpacesAndLowerCase(str)) {
+    charMap.set(char, charMap.get(char) + 1 || 1);
+  }
+  return charMap;
+}
+
+const removeSpacesAndLowerCase = (str) => {
+  return str.replaceAll(' ', '').toLowerCase();
+}
+
+//solve in 2 lines of code
+const anagrams = (strA, strB) => {
+    return cleanString(strA) === cleanString(strB);
+}
+//use function for repeanable code
+const cleanString = (str) => {
+    return str.toLowerCase().replace(" ", '').split('').sort().join('');
+}
 module.exports = anagrams;
+
+
