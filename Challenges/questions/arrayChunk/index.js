7152d2f46b01402f04f15dd1adb5eeff02b6e543
Maurice <mauriceglenn2@gmail.com>
2023-12-04 10:50:57 -0600

    learning arrayChunk

--- Challenges/questions/arrayChunk/index.js
+++ Challenges/questions/arrayChunk/index.js
@@ -5,10 +5,24 @@
  * arrayChunk([0, 1, 2, 3], 2) === [[0, 1], [2, 3]]
  * arrayChunk([0, 1, 2, 3, 4], 2) === [[0, 1], [2, 3], [4]]
  * arrayChunk([0, 1, 2, 3, 4], 3) === [[0, 1, 2], [3, 4]]
+ * 
+ * create an empty array to hold chunks called chunkedArray
+ * for each index in the array that is divisible by size ([0, 1, 2, 3, 4], 2) this would be 0 and 2 and 4
+ * create a subarray of lenght "size" that slices from the current index
+ * push the subarray into the chunkedArray
  */
 
 const arrayChunk = (array, size) => {
-  
+//create an empty array to hold chunks called chunkedArray
+  const chunkedArray = [];
+//for each index in the array that is divisible by size
+  for(let i = 0; i < array.length; i += size) {
+    //create a subarray of lenght "size" that slices from the current index
+    const subarray = array.slice(i, i + size);
+    //push the subarray into the chunkedArray
+    chunkedArray.push(subarray);
 };
+    return chunkedArray;
+}
 
 module.exports = arrayChunk;
